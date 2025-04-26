fba544a7e3596b930ca0d13d653b7374be8359e2
huyvo79 <vohaanhhuy79@gmail.com>
2025-04-26 14:36:55 +0700

    update newww

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,36 +2,38 @@
 <html>
 <head>
     <title>Laravel 10.48.0 - CRUD User Example</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
     <script type="text/javascript" src="{{ asset('js/scripts.js') }}"></script>
 </head>
 <body>
-<nav class="navbar navbar-light navbar-expand-lg mb-5" style="background-color: #e3f2fd;">
-    <div class="container">
-        <a class="navbar-brand mr-auto" href="#">Laravel Training</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                @guest
+<nav class="navbar navbar-expand-lg bg-body-tertiary border border-2 border-dark">
+        <div class="container-fluid">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03"
+                aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse  " id="navbarTogglerDemo03">
+                <ul class="navbar-nav me-auto mx-auto mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ route('login') }}">Login</a>
+                        <a class="nav-link active" aria-current="page" href="{{ route('login') }}">Login</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ route('user.createUser') }}">Create user</a>
+                        <a class="nav-link" href="{{ route('user.createUser') }}">Create</a>
                     </li>
-                @else
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ route('signout') }}">Logout</a>
+                        <a class="nav-link "  href="{{ route('signout') }}" >LogOut</a>
                     </li>
-                @endguest
-            </ul>
+                </ul>
+                <!-- <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form> -->
+            </div>
         </div>
-    </div>
-</nav>
+    </nav>
+
 @yield('content')
 </body>
 </html>
